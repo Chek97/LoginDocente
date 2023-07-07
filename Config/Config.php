b02750c336b01dd51a24d0a1c51ef21f6dee05a1
Cristian Felipe Checa Rojas <felychk203@hotmail.com>
2023-07-07 02:37:29 -0500

    cambio de variables de conexion bd

--- Config/Config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    
-    namespace Config;
-
-    define("DB_HOST", "localhost");
-    define("DB_NAME", "login-docente");
-    define("DB_USER", "root");
-    define("DB_PASSOWORD", "");
-?>
--- Model/Conection/Conection.php
+++ Model/Conection/Conection.php
@@ -9,14 +9,22 @@
     class Conection {
         
         private $conn;
+        private $host;
+        private $dbName;
+        private $user;
+        private $password;
 
         public function __construct(){
+            $this->host = "localhost";
+            $this->dbName = "login-docente";
+            $this->user = "root";
+            $this->password = "";
             $this->getConnection();
         }
 
         public function getConnection(){
             try {
-                $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName, $this->user, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->conn->exec("SET CHARACTER SET utf8");
             } catch (PDOException $th) {
